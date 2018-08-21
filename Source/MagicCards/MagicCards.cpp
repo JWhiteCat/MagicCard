a159c8fed34f078ebd996bffe0c898783356a411
DESKTOP-H5N22RS\good <1173653942@qq.com>
2018-08-21 18:15:27 +0800

    第一次提交

---
+++ Source/MagicCards/MagicCards.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MagicCards.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MagicCards, "MagicCards" );
---
+++ Source/MagicCards/MagicCardsGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MagicCardsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGICCARDS_API AMagicCardsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
